bac5aa09b897c08e8ff140ce8484ee494c1a5d98
mtayyababdullah <149929072+mtayyababdullah@users.noreply.github.com>
2023-11-30 00:25:13 +0500

    Add files via upload

--- task2.js
+++ task2.js
@@ -2,22 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var prompt = require("prompt-sync")();
 //Task 2:
-//Scenario 1: Sum numbers
+console.log('Scenario 1: Sum numbers');
 let addition = (c, d) => c + d;
 console.log(addition(50, 25));
 //Scenario 2: Check Even or Odd
 let num = prompt("Please enter a number ");
 if (num % 2 == 0) {
-    console.log("It is Even");
+    console.log(`${num} is Even`);
 }
 else {
-    console.log("It is odd");
+    console.log(`${num} is odd`);
 }
 //Scenario 3: Calculate Area:
 function rectangleArea(width, height) {
     return width * height;
 }
-console.log(rectangleArea(5, 7));
+let width = prompt("Please enter width: ");
+let height = prompt("Please enter height: ");
+console.log(`Total area of rectagle is : ${rectangleArea(width, height)}`);
 //Scenario 4: String Reversal
 function reverse(str) {
     // split the name into an array
@@ -25,13 +27,13 @@ function reverse(str) {
     // join the name back
     return str.split('').reverse().join('');
 }
-let original = "Tayyab";
+let original = prompt("Please enter a name: ");
 let reversestring = reverse(original);
 console.log(reversestring);
 // Scenario 5: Temperature Conversion:
+let tempC = prompt("Please enter celsius Temperature ");
 function convertCelsiusToFahrenheit(celsius) {
     return (celsius * 9 / 5) + 32;
 }
-let tempC = 20;
 let tempF = convertCelsiusToFahrenheit(tempC);
 console.log(`${tempC}°C is equal to ${tempF}°F.`);
--- task2.ts
+++ task2.ts
@@ -8,23 +8,32 @@ let  addition = (c: number, d: number) => c + d;
 
 console.log (addition(50,25));
 
+
+
 //Scenario 2: Check Even or Odd
 
 let num = prompt("Please enter a number ");
 
 if (num % 2 == 0) {
-    console.log("It is Even");
+    console.log(`${num} is Even`);
   } else {
-    console.log("It is odd");
+    console.log(`${num} is odd`);
   }
+
+
+
 
   //Scenario 3: Calculate Area:
 
 function rectangleArea(width: number, height: number) {
     return width * height;
   }
+  let width = prompt("Please enter width: ");
+  let height = prompt("Please enter height: ");
+    console.log(`Total area of rectagle is : ${rectangleArea(width, height)}`)
 
-    console.log(rectangleArea(5, 7))
+
+
 
 
     //Scenario 4: String Reversal
@@ -39,12 +48,15 @@ function rectangleArea(width: number, height: number) {
 
     }
 
-    let original = "Tayyab"
+    let original = prompt("Please enter a name: ");
     let reversestring = reverse(original)
 
     console.log(reversestring)
 
 
+
+
+    
    // Scenario 5: Temperature Conversion:
 
    let tempC = prompt("Please enter celsius Temperature ");
@@ -54,6 +66,5 @@ function convertCelsiusToFahrenheit(celsius: number): number {
     return (celsius * 9 / 5) + 32;
 }
 
-//let tempC: number = 20;
 let tempF: number = convertCelsiusToFahrenheit(tempC);
 console.log(`${tempC}°C is equal to ${tempF}°F.`);
